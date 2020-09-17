46f92b8f1f42903641a565e6a7a7be48d364c2c1
ASDAFF <asdaff.asad@yandex.ru>
2020-09-17 15:57:44 +0500

    source

--- source/dev2fun.authemail/install/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-$arModuleVersion = array(
-    "VERSION" => "1.0.4",
-    "VERSION_DATE" => "2020-03-04 10:00:00",
-);
--- source/dev2fun.authemail/lang/ru/options.php
+++
@@ -1,3 +0,0 @@
-<?php
-$MESS["D2F_AUTH_EMAIL_KEYS"] = "Поддерживаемые ключи";
-$MESS["D2F_AUTH_EMAIL_LABEL_ADD"] = "Добавить";
